70e743b2077bdf39f325489118aad5a510511f0f
Sakolkiat <sakolkiat.n@gmail.com>
2025-10-02 02:04:39 +0700

    feat: add create ocean grid function
    - This function will create an array that contains specified items

--- src/modules/game.js
+++ src/modules/game.js
@@ -4,3 +4,17 @@ function Gameboard() {
 	// track missed attacks
 	// report if all the ships have sunk
 }
+
+function oceanGrid() {
+	const FIELD_SIZE = 10;
+	let oceanColumn = Array.from(new Array(FIELD_SIZE), () => []);
+
+	return oceanColumn;
+}
+
+let test = oceanGrid();
+console.log(test);
+
+// function placeShipHorizontal() {
+
+// }
