29e8cfe43ff879ad458165d35c2f1ac1a5fadc13
Sakolkiat <sakolkiat.n@gmail.com>
2025-10-01 17:35:09 +0700

    test: categorize test description

--- src/test/ship.test.js
+++ src/test/ship.test.js
@@ -2,37 +2,42 @@ import { Ship } from "../modules/ship.js";
 
 describe('Ship factory function', () => {
 
-	test('ship takes hit', () => {
-		const ship = Ship(3);
-		expect(ship.showHit()).toBe(0);
+	describe('Methods', () => {
+		test('ship takes hit', () => {
+			const ship = Ship(3);
+			expect(ship.showHit()).toBe(0);
 
-		ship.getHit();
-		expect(ship.showHit()).toBe(1);
-		ship.getHit();
-		expect(ship.showHit()).toBe(2);
-	});
+			ship.getHit();
+			expect(ship.showHit()).toBe(1);
+			ship.getHit();
+			expect(ship.showHit()).toBe(2);
+		});
 
-	test('ship sink when hit enough times', () => {
-		const ship = Ship(2);
+		test('ship sink when hit enough times', () => {
+			const ship = Ship(2);
 
-		ship.getHit();
-		expect(ship.isSunk()).toBe(false);
+			ship.getHit();
+			expect(ship.isSunk()).toBe(false);
 
-		ship.getHit();
-		expect(ship.isSunk()).toBe(true);
-	});
+			ship.getHit();
+			expect(ship.isSunk()).toBe(true);
+		});
 
-	test('Throw input type error', () => {
-		expect(() => Ship('not number')).toThrow();
-		expect(() => Ship(true)).toThrow();
-		expect(() => Ship(null)).toThrow();
-	});
+	})
+	describe('Error handling', () => {
+		test('Throw input type error', () => {
+			expect(() => Ship('not number')).toThrow();
+			expect(() => Ship(true)).toThrow();
+			expect(() => Ship(null)).toThrow();
+		});
 
-	test('Throw if not integer', () => {
-		expect(() => Ship(1.5)).toThrow();
-		expect(() => Ship(0)).toThrow();
-		expect(() => Ship(-1)).toThrow();
-		expect(() => Ship(-1.5)).toThrow();
+		test('Throw if not integer', () => {
+			expect(() => Ship(1.5)).toThrow();
+			expect(() => Ship(0)).toThrow();
+			expect(() => Ship(-1)).toThrow();
+			expect(() => Ship(-1.5)).toThrow();
+		});
+
 	});
 
 
