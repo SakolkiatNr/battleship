a123ebd2846698067b3fcbb69d0b77e497b04961
Sakolkiat <sakolkiat.n@gmail.com>
2025-10-04 04:09:47 +0700

    refactor: change check coordination to cover the edges

--- src/modules/game.js
+++ src/modules/game.js
@@ -5,6 +5,8 @@ function Gameboard() {
 	// receive attack function
 	// track missed attacks
 	// report if all the ships have sunk
+
+	let board = oceanGrid();
 }
 
 export function oceanGrid() {
@@ -22,9 +24,10 @@ function placeHorizontalShip(board, start, ship) {
 	const end = ship.length;
 	const [row, col] = start;
 
-	checkHorizontal(board, start, ship.length);
-	for (let i = 0; i < end; i++) {
-		board[row][col + i] = shipID;
+	if (checkHorizontal(board, start, ship.length)) {
+		for (let i = 0; i < end; i++) {
+			board[row][col + i] = shipID;
+		}
 	}
 }
 
@@ -32,8 +35,9 @@ function checkHorizontal(board, start, shipLength) {
 	const [row, col] = start;
 
 	// if out of range
-	if (row < 0 || row > 9) return false;
-	if (col + shipLength - 1 > 9) return false;
+	if (!(col >= 0 && col <= 9) ||
+		!(row >= 0 && row <= 9) ||
+		(col + shipLength - 1 > 9)) return false;
 
 	// target cells must be empty
 	for (let i = 0; i < shipLength; i++) {
@@ -48,19 +52,19 @@ function placeVerticalShip(board, start, ship) {
 	const [row, col] = start;
 
 	//check vertical case
-	checkVertical(board, start, ship.length)
-
-	for (let i = 0; i < end; i++) {
-		board[row + i][col] = shipID;
+	if (checkVertical(board, start, ship.length)) {
+		for (let i = 0; i < end; i++) {
+			board[row + i][col] = shipID;
+		}
 	}
 }
 
 function checkVertical(board, start, shipLength) {
 	const [row, col] = start;
-
 	// if out of range
-	if (col < 0 || col > 9) return false;
-	if (row + shipLength - 1 > 9) return false;
+	if (!(col >= 0 && col <= 9) ||
+		!(row >= 0 && row <= 9) ||
+		(row + shipLength - 1 > 9)) return false;
 
 	// target cells must be empty
 	for (let i = 0; i < shipLength; i++) {
@@ -74,7 +78,13 @@ function checkVertical(board, start, shipLength) {
 let boat = Ship(3);
 let boat2 = Ship(2);
 let board = oceanGrid();
-placeHorizontalShip(board, [9, 4], boat);
-placeVerticalShip(board, [7, 0], boat2);
-
+placeHorizontalShip(board, [9, 0], boat);
+placeVerticalShip(board, [0, 0], boat2);
 console.log(board);
+
+// const ships = {
+// ship.id : ship(n)
+// "id1": 'ship1',
+// "id2": 'ship2',
+// "id3": 'ship3',
+// }
