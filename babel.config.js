6e460218083acce29e0db2031d52520158b506f6
Sakolkiat <sakolkiat.n@gmail.com>
2025-10-04 05:06:03 +0700

    fix: remove type module for babel
    - this commit will fix error when run test in ES6

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,6 @@ module.exports = {
 	presets: [["@babel/preset-env", { targets: { node: "current" } }]],
 };
 
-{
-	"presets": ["@babel/preset-env"]
-}
+// {
+// "presets": ["@babel/preset-env"]
+// }
