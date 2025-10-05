8942441b3f1a613381647c24356fa3b18395b976
Sakolkiat <sakolkiat.n@gmail.com>
2025-10-05 21:47:44 +0700

    test: test createShips

---
+++ src/test/createShips.test.js
@@ -0,0 +1,25 @@
+
+import { createShips } from "../modules/createShips.js";
+
+describe('Create ship', () => {
+	let ships;
+
+	beforeEach(() => {
+		ships = createShips();
+	});
+
+	describe('Create ships', () => {
+		test('contains five ships', () => {
+			expect(Object.keys(ships).length).toBe(5);
+		});
+
+		test('ship is an object:', () => {
+			for (let key in ships) {
+				const ship = ships[key];
+				expect(ship.showHit).toBe(0);
+				expect(ship.isSunk).toBe(false);
+			}
+		});
+	});
+
+});
