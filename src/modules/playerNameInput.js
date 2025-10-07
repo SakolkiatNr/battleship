c78fe447a757757cc97f939776d8d13ed3feebe6
Sakolkiat <sakolkiat.n@gmail.com>
2025-10-07 21:26:07 +0700

    style: remove comments and blank space

--- src/modules/playerNameInput.js
+++ src/modules/playerNameInput.js
@@ -1,11 +1,6 @@
 import { Player } from "./player.js";
-// player input
-// store in variables
-
 
 export function createPlayer(name) {
-	// const input = document.getElementById('player-name');
-
 	return Player(name);
 }
 
