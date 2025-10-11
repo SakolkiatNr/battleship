4587ff0b6e11c52e199334b828e339c5e1daefcf
Sakolkiat <sakolkiat.n@gmail.com>
2025-10-11 21:05:27 +0700

    chore: remove comments

--- src/modules/gameFlow.js
+++ src/modules/gameFlow.js
@@ -132,32 +132,3 @@ export function newGame() {
 		isPlayerTurn = true;
 	});
 }
-
-// function playerAttack(e, player, ai) {
-// 	const cell = e.target.closest('button');
-//
-// 	if (!cell || !cell.dataset.row || !cell.dataset.col) return;
-// 	if (cell.dataset.val === '0' || cell.dataset.val === '1') return;
-//
-// 	const hit = attackAI(cell, ai);
-// 	hit
-// 		? reportAttackStatus('playerHit', player.name, ai.name)
-// 		: reportAttackStatus('playerMiss', player.name, ai.name);
-//
-// 	return true;
-// }
-//
-// function aiAttack(ai, player) {
-//
-// 	return new Promise(resolve => {
-// 		const hit = attackPlayer(player);
-//
-// 		setTimeout(() => {
-// 			hit
-// 				? reportAttackStatus('enemyHit', ai.name, player.name)
-// 				: reportAttackStatus('enemyMiss', ai.name, player.name);
-//
-// 			resolve();
-// 		}, 800);
-// 	});
-// }
